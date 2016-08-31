56921780008fa939530c30a4a456b1581a0db324
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-08-31 11:56:55 +0300

    ::all_issues and ::list_owner_issues work fine

--- lib/gh_issues.rb
+++ lib/gh_issues.rb
@@ -4,24 +4,38 @@ require 'octokit'
 
 module GhIssues
   class GitHubAccessTokenError < StandardError; end
+  class InvalidOwnerError < StandardError; end
   
   def self.ghi_token_defined?
     raise ::GhIssues::GitHubAccessTokenError, "Please define GH_ISSUES_TOKEN environment variable." unless ENV['GH_ISSUES_TOKEN']
     true
+  end
+
+  def self.list_owner_issues(requested_owner)
+    if ::GhIssues.ghi_token_defined?
+      all_issues = ::GhIssues.all_issues
+      if all_issues[requested_owner]
+        all_issues[requested_owner]
+      else
+        raise ::GhIssues::InvalidOwnerError, "Invalid owner requested"
+      end
+    end
   end
   
   def self.all_issues
     if ::GhIssues.ghi_token_defined?
       client = Octokit::Client.new(:access_token => ENV['GH_ISSUES_TOKEN'])
       client.auto_paginate = true
-      open_issues = []
+      open_issues = {}
       client.repos.each do |repo|
         if repo[:open_issues_count] > 0
+          owner = repo[:owner][:login]
+          open_issues[owner] = [] unless open_issues[owner]
           data = {
             full_name: repo[:full_name],
             open_issues_count: repo[:open_issues_count],
           }
-          open_issues << data
+          open_issues[owner] << data
         end
       end
       open_issues
--- lib/gh_issues/cli.rb
+++ lib/gh_issues/cli.rb
@@ -12,7 +12,7 @@ module GhIssues
       puts GhIssues::VERSION
     end
     
-    desc "all", "List all open issues"
+    desc "all", "List all open issues, grouped by owner"
     option :sort_by, default: 'name', banner: "count"
     option :sort_order, default: 'asc', banner: "desc"
     def all
@@ -21,34 +21,73 @@ module GhIssues
       sort_order = ['asc', 'desc'].include?(options[:sort_order]) ? options[:sort_order] : 'asc'
       all_issues = GhIssues.all_issues
       if all_issues.count > 0
-        all_issues.sort_by!{|item| item[:open_issues_count]} if sort_by == 'count'
-        all_issues.reverse! if sort_order == 'desc'
-        puts print_table(style: 'issues',
-                         title: 'All Open Issue(s)',
-                         headings: ['Repo', 'Issue Count'],
-                         rows: all_issues.map{|item| [item[:full_name], item[:open_issues_count]]})
+        puts print_issue_list(all_issues, sort_by: sort_by, sort_order: sort_order)
       else
-        puts "You have no issue to display..."
+        puts "Hooray! You have no issue to display..."
       end
     end
-    
+
+    desc "list OWNER_NAME", "List issues belongs to owner"
+    option :sort_by, default: 'name', banner: "count"
+    option :sort_order, default: 'asc', banner: "desc"
+    def list(owner)
+      ENV['GH_ISSUES_COLORIZE'] = '1' if options[:color]
+      sort_by = ['name', 'count'].include?(options[:sort_by]) ? options[:sort_by] : 'name'
+      sort_order = ['asc', 'desc'].include?(options[:sort_order]) ? options[:sort_order] : 'asc'
+      begin
+          owner_issues = GhIssues.list_owner_issues(owner)
+      rescue InvalidOwnerError => error_message
+        puts error_message
+        exit
+      end
+      if owner_issues.count > 0
+        m = {}
+        m[owner] = owner_issues
+        puts print_issue_list(m, sort_by: sort_by, sort_order: sort_order)
+      else
+        puts "Hooray! You have no issue to display..."
+      end
+    end
     
     no_commands do
-      def print_table(**options)
-        table_style = options[:style]
-        table_title = ENV['GH_ISSUES_COLORIZE'] ? set_color(options[:title], :yellow) : options[:title]
-        table_headings = ENV['GH_ISSUES_COLORIZE'] ? options[:headings].map{|t| set_color(t, :white) } : options[:headings]
-        table_rows = options[:rows]
-        if ENV['GH_ISSUES_COLORIZE']
-          case table_style
-          when 'issues'
-            table_rows.map!{|r| [set_color(r[0], :white), set_color(r[1], :green)]}
+      def print_issue_list(data, **options)
+        sort_by = options[:sort_by]
+        sort_order = options[:sort_order]
+        all_issues_sum = 0
+        table = Terminal::Table.new do |t|
+          data.each do |owner, items|
+            items.sort_by!{|item| item[:open_issues_count]} if sort_by == 'count'
+            items.reverse! if sort_order == 'desc'
+            t.add_row [{value: "#{colorize(owner, :yellow)}", colspan: 2, alignment: :center}]
+            t.add_separator
+            items.map{|item| t.add_row([colorize(item[:full_name], :cyan), {value: colorize(item[:open_issues_count], :white), alignment: :right}])}
+            issues_sum = items.map{|item| item[:open_issues_count]}.inject(0){|sum,x| sum + x }
+            all_issues_sum = all_issues_sum + issues_sum
+            t.add_separator
+            t.add_row [{value: colorize(issues_sum, :green), colspan: 2, alignment: :right}]
+            t.add_separator
           end
         end
-        Terminal::Table.new :headings => table_headings,
-                            :rows => table_rows,
-                            :title => table_title
+        table.add_row [colorize('Total amount', :white), {value: colorize(all_issues_sum, :white), alignment: :right}]
+        table
+      end
+      
+      def colorize(text, color)
+        if ENV['GH_ISSUES_COLORIZE']
+           set_color(text, color)
+        else
+          text
+        end
+      end
+      
+      def pluralize(singular, plural, number)
+        if number > 1
+          plural
+        else
+          singular
+        end
       end
     end
+
   end
 end
